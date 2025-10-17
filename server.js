39437e9651ce3091a048df19b145bcdf99787aa8
sridharmurugantpm <sridhar.murugan.tpm@gmail.com>
2025-10-17 11:09:39 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -9,11 +9,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(cors({
-  origin: ["http://localhost:3000", ""],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true
-}));
+app.use(cors({ origin: "*" }));
+
 
 // MongoDB Connection
 const startMongo = async () => {
